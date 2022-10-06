dc27f20910b558e782c0e6c3db6807160b7ba548
Clayton J Barnette <clayton.barnette@gsa.gov>
2022-10-06 12:52:26 -0400

    Docker CPCPT Tool - Demo

--- docker-source/profiles/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class UploadFileForm(forms.Form):
-    file = forms.FileField()
-    profile = forms.CharField()
-    type = forms.CharField()
-    version = forms.CharField()
