3135f5da2f2442d47602a09c701276a78aecb930
Clayton J Barnette <clayton.barnette@gsa.gov>
2022-10-07 16:43:24 -0400

    initial commit

--- start.cmd
+++ start.cmd
@@ -5,26 +5,41 @@ set instance=%RANDOM%
 
 sleep 2
 @REM Build the docker image only, the image is started in the next step 
-set message1=Status: Starting Build of the Docker Desktop CPCT Tool Image
+set message1=Status: Starting build of the Docker Desktop CPCT Tool Image
 echo %message1%
+echo;
 docker build -t desktop-cpct:%instance% .
+echo;
+set message1=Status: Docker Image created... 
+echo %message1%
+echo;
 
-sleep 5
+sleep 10
 @REM Run the Docker Image on computer's private port 127.0.0.1:8000
 @REM exposing port 8000 to the computer/web browser.  
 set message2=Status: Starting the Docker Image Tagged as: desktop-cpct:%instance% 
 echo %message2%
+echo;
+
 set message2=Status: Your Docker Image ID is:  
 echo %message2%
 docker run -d -p 127.0.0.1:8000:8000 desktop-cpct:%instance%
 echo;
 set message2=Status: Your Docker Image is running...
 echo %message2% 
+echo;
 
-sleep 10
+sleep 5
 @REM Start the default Web Browser
 set message3=Status: Opening CPCT in your default Web Browser
 echo %message3%
+echo;
+set message3=Status: Thank you for using the Docker Desktop CPCT Tool.
+echo %message3%
+set message3=    (This window will automatically closed)
+echo %message3%
+sleep 15
+
 start http://127.0.0.1:8000
 
 
