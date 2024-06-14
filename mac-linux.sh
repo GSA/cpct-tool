c34c3a99ff2995859716d4f079d85a6e65b4cb60
Clayton J Barnette <110616975+claytonjbarnette@users.noreply.github.com>
2024-06-14 11:25:34 -0400

    CPCT Tool v2.0.1
    New installer for Mac and Linux (BETA)

---
+++ mac-linux.sh
@@ -0,0 +1,26 @@
+# Mac CPCT Tool Installer for Docker Desktop
+echo " "
+echo "This will install the CPCT Tool v2.0.1 for Docker Desktop"
+echo "Please make sure you have Docker Desktop running"
+echo " "
+sleep 20
+# Start building the Docker Image 
+echo "Building Docker image for the CPCT Tool v2.0.1 ..."
+docker  build -t cpct-tool-v2:latest .
+echo " "
+sleep 10
+# Share the name of the Docker Image to look for in Docker Desktop
+echo "The image name in Docker Desktop for the CPCT Tool is: cpct-tool-v2:latest"
+echo " "
+echo "Installation of the CPCT Tool is complete."
+echo " "
+sleep 10
+echo "Please wait... starting the CPCT Tool 2.0.1"
+docker run -d -p 127.0.0.1:8000:8000 -t cpct-tool-v2:latest
+echo "Installation of the CPCT Tool is complete."
+echo " "
+sleep 10
+echo "Please visit: http://127.0.0.1:8000" in your browser to use the tool.
+echo " "
+echo "Thank you for using the Dockerized Desktop version of the CPCT Tool"
+echo " "
